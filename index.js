b12eaaf46a468f872ae4c1026d8d45fcfbafa69f
Preetipan <preetipan.s@ku.th>
2024-11-11 23:52:09 +0700

    บช

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// index.js
 const express = require("express");
-const line = require('@line/bot-sdk');
+const line = require("@line/bot-sdk");
 require("dotenv").config();
 
-// create LINE SDK config from env variables
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
+// Create LINE SDK client
 const client = new line.messagingApi.MessagingApiClient({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 });
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
+// Endpoint for LINE webhook
+app.post("/callback", line.middleware(config), (req, res) => {
+  // Process incoming events
+  Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
@@ -29,39 +24,150 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-app.get('/' , (req, res) => {
-  res.send('sqqqqqqqqq')
-})
-
-// event handler
+// Handle incoming events from LINE platform
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+  // Check if the event is from a group or room
+  if (event.source.type === "group" || event.source.type === "room") {
+    // If the message is not a text message, do nothing
+    if (event.type !== "message" || event.message.type !== "text") {
+      return Promise.resolve(null);
+    }
+
+    const userMessage = event.message.text.trim();
+
+    //เลขบัญชี
+    if (userMessage === "บช") {
+
+      const carouselMessage = {
+        type: "flex",
+        altText: "บัญชีธนาคาร",
+        contents: {
+          type: "bubble",
+          direction: "ltr",
+          header: {
+            type: "box",
+            layout: "horizontal",
+            backgroundColor: "#2D0287FF",
+            borderColor: "#FFFFFF02",
+            contents: [
+              {
+                type: "text",
+                text: "กลุ่มไก่ชน DTG",
+                weight: "bold",
+                size: "xl",
+                color: "#FFFFFFFF",
+                align: "center",
+                gravity: "center",
+                margin: "none",
+              },
+            ],
+          },
+          hero: {
+            type: "image",
+            url: "https://s.isanook.com/mn/0/ud/92/460465/761984-01.jpg?ip/crop/w1200h700/q80/webp",
+            size: "full",
+            aspectRatio: "20:13",
+            aspectMode: "cover",
+            action: {
+              type: "uri",
+              label: "Action",
+              uri: "https://linecorp.com/",
+            },
+          },
+          body: {
+            type: "box",
+            layout: "vertical",
+            spacing: "md",
+            contents: [
+              {
+                type: "box",
+                layout: "vertical",
+                flex: 1,
+                contents: [
+                  {
+                    type: "text",
+                    text: "เลขบัญชี",
+                    size: "lg",
+                    color: "#444444FF",
+                  },
+                  {
+                    type: "text",
+                    text: "9922120029",
+                    weight: "bold",
+                    size: "xxl",
+                    color: "#FF0A0AFF",
+                    align: "center",
+                    gravity: "center",
+                    margin: "md",
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "text",
+                    text: "ชื่อบัญชี",
+                    size: "lg",
+                    color: "#444444FF",
+                    gravity: "center",
+                    margin: "sm",
+                  },
+                  {
+                    type: "text",
+                    text: "ปรีติพันธ์ สุทธิพันธ์",
+                    weight: "bold",
+                    size: "xl",
+                    color: "#444444FF",
+                    align: "center",
+                    margin: "md",
+                  },
+                ],
+              },
+              {
+                type: "button",
+                action: {
+                  type: "message",
+                  label: "คัดลอก",
+                  text: "9922120029",
+                },
+                color: "#625C5CFF",
+                height: "md",
+                style: "primary",
+                gravity: "center",
+              },
+            ],
+          },
+          footer: {
+            type: "box",
+            layout: "horizontal",
+            contents: [
+              {
+                type: "text",
+                text: "ชื่อตรงกับบัญชีนี้บัญชีเดียวเท่านั้น",
+                weight: "bold",
+                size: "xs",
+                color: "#FF0000FF",
+                align: "center",
+                gravity: "center",
+              },
+            ],
+          },
+        },
+      };
+
+      return client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [carouselMessage],
+      });
+    }
   }
-
-  const messageText = event.message.text;
-
-  // เช็คข้อความว่ามีคำว่า "mem" หรือไม่
-  if (messageText.toLowerCase().includes("mem")) {
-    const replyMessage = { type: 'text', text: 'สวัสดีสมาชิก' };
-
-    // ใช้ API สำหรับตอบกลับข้อความ
-    return client.replyMessage(event.replyToken, replyMessage);
-  }
-
-  // create an echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [echo],
-  });
+  return Promise.resolve(null);
 }
 
-// listen on port
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+  console.log(`Listening on ${port}`);
 });
