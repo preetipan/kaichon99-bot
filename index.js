645e9657664095ff34ae6d7807739b296e777d52
preetipan <preetipan.s@ku.th>
2025-02-05 14:41:25 +0700

    edit new server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use("/taiga689/webhook/img", express.static('./Img'));
 
 app.use("/taiga689/webhook/botAlert", botAlert);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 1689;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}`);
 });
